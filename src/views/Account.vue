53bb13e341c9246a705413179adb25a7a157759f
Ikernee <ikernesanz@gmail.com>
2023-02-10 20:23:39 +0100

    account with split test commented

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -1,6 +1,8 @@
 <template>
   <Nav />
-  <h1>Name: {{username}}</h1>
+  <!-- <h1>Name: {{username.split("@")[0]}}</h1> -->
+  <!-- <h1>Handle: @{{username.split("@")[0]}}</h1> -->
+  <h1>Email: {{username}}</h1>
   <img :src="avatar_url ? avatar_url : 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__480.png'" alt="Profile picture">
 </template>
 
